e49e9eb1ce8c13e2fd3e16b3fa894c2629a3f2c7
VyrekXD <vyrekxd@gmail.com>
2022-04-26 16:52:59 -0500

    Fix test

--- test/index.js
+++ test/index.js
@@ -7,12 +7,9 @@ import { join, dirname } from 'path'
 
 //Import fs to write the gif maked, this step is optional
 import fs from 'fs/promises'
-<<<<<<< HEAD
 
 // To fetch the buffer of an image
-import fetch from 'node-fetch'
-=======
->>>>>>> 5bbdc258e42e1025561e04183d51688ae0be45b7
+import phin from 'phin'
 
 //This is for get the dirname
 // @ts-ignore
@@ -23,12 +20,11 @@ const __dirname = dirname(__filename)
 ;(async () => {
 	// This is an example
 	// Fetch the buffer of an image
-	const res = await fetch(
+	const res = await phin(
 		'https://cdn.discordapp.com/attachments/724014357343895703/960220310144184330/unknown.png'
 	)
 
 	//We instance the class Gif and give the proportions of width 500 and height 500
-<<<<<<< HEAD
 	const myGif = new Gif(500, 500)
 	//We set 3 images that will be 3 frames
 	await myGif.setFrames([
@@ -36,7 +32,7 @@ const __dirname = dirname(__filename)
 			src: 'https://cdn.discordapp.com/attachments/960206787775201314/960213088974561280/unknown.png'
 		},
 		{
-			src: new Uint8Array(await res.arrayBuffer())
+			src: new Uint8Array(Buffer.from(res.body))
 		},
 		{
 			src: 'https://cdn.discordapp.com/attachments/960206787775201314/960213089536585808/unknown.png',
@@ -46,27 +42,8 @@ const __dirname = dirname(__filename)
 	])
 
 	//Render the image, it will return a Buffer or it will give an error if anything goes wrong
-	const render = await myGif.decode()
+	const render = await myGif.encode()
 
 	//Writes the gif in this folder
 	await fs.writeFile(join(__dirname, 'make-a-gif.gif'), render)
-=======
-	const Image = new Gif(500, 500)
-		//This is the delay between frames
-		.setDelay(1500)
-		.setRepeat(0)
-		//We set 3 images that will be 3 frames
-		.setFrames([
-			'https://i.imgur.com/QnaDhkD.png',
-			'https://i.imgur.com/8bazwQp.png',
-			'https://i.imgur.com/wPMwvr5.png'
-		])
-
-	//Render the image, it will return a Buffer or it will give an error if anything goes wrong
-	const Render = await Image.render().catch((e) => console.error(e))
-	if (!Render) return
-
-	//Writes the gif in this folder
-	await fs.writeFile(join(__dirname, 'make-a-gif.gif'), Render)
->>>>>>> 5bbdc258e42e1025561e04183d51688ae0be45b7
 })()
